83d8a8731638f39732e9d2187f37f7ca170ab23a
Denis Siliukov <abhem21rus@mail.ru>
2024-03-04 09:32:16 +0300

    Project initialization

---
+++ src/main/java/com/swp/schoolinfo/SchoolInfoApplication.java
@@ -0,0 +1,13 @@
+package com.swp.schoolinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolInfoApplication.class, args);
+	}
+
+}
